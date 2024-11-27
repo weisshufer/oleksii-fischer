f51a59fbb2fe451fb58bdfb41c52b1acd2b0cec0
Oleksii Fischer <alex@weisshufer.digital>
2024-11-27 09:19:09 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
   webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
